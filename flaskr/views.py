38ea4cc431f5e555e515f50ed11185c715dc4f63
LobotomyInfoStorehouse <LobotomyInfoStorehouse@gmail.com>
2018-10-31 01:49:13 +0900

    fix

--- flaskr/views.py
+++ flaskr/views.py
@@ -2,5 +2,6 @@ from flask import request, redirect, url_for, render_template, flash
 from flaskr import app
 
 @app.route('/')
-def show_entries():
+def index():
+    print ('run')
     return render_template('index.html')
