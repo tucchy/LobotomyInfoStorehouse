be9af5993345182c48a84365133fa822a9592724
LobotomyInfoStorehouse <LobotomyInfoStorehouse@gmail.com>
2018-10-31 23:54:34 +0900

    fix run

--- manage.py
+++ manage.py
@@ -1,15 +1,7 @@
 import os
-<<<<<<< HEAD
 from flaskr import app
 
 port = os.environ.get("PORT","5000")
 
-app.run()
-#app.run(host = "0.0.0", port = int(port))
-=======
-from flaskr import app
-
-port = os.environ.get("PORT","5000")
-
-app.run(host = “0.0.0”, port = int(port))
->>>>>>> deaf5170b67f2fbaea7ec5638cdc36dc47b81ce0
+#app.run()
+app.run(host = "0.0.0", port = int(port))
